a93b94a08b968d03a739330fe707ec4a09669ec0
Andrey <andreykvital@gmail.com>
2016-02-23 21:52:23 -0300

    Ensure stuff

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const Chrome = require('chrome-remote-interface')
 const YouTube = require('./YouTube')
 
 Chrome.List({}, (err, list) => {
+  if (err) {
+    console.error(err.message)
+    return
+  }
+
   const tabs = list.filter(tab => tab.type === 'page').map((tab, index) => {
     return {
       index,
@@ -12,6 +17,11 @@ Chrome.List({}, (err, list) => {
   })
 
   const youtube = tabs.filter(tab => /(www\.)?youtube.com/.test(tab.url))
+
+  if (youtube.length === 0) {
+    process.stdout.exit(0)
+    return
+  }
 
   Chrome({ chooseTab: () => youtube[0].index }, chrome => {
     chrome.Runtime.evaluate({
